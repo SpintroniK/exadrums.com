fb599bd5c0c739771b8b26f55752e6503a12a2b6
oden.jonathan@gmail.com <oden.jonathan@gmail.com>
2021-05-12 06:20:40 +0200

    Add generic css

--- src/Main.vue
+++ src/Main.vue
@@ -3,5 +3,28 @@
 </template>
 
 <style>
+:root {
+  --clr-primary: #0e232e;
+  --clr-secondary: #364e59;
+  --clr-ternary: #eeefed;
+  --clr-ascent: #ef2929;
+  --clr-background: white;
+  --spacer: 1rem;
+}
+
+*,
+* ::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+html,
+body {
+  width: 100%;
+  height: 100%;
+  scroll-behavior: smooth;
+}
 
 </style>
