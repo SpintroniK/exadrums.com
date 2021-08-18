91fada878892b44b401df9b0a65b790748fcf0e5
oden.jonathan@gmail.com <oden.jonathan@gmail.com>
2021-08-18 08:37:11 +0200

    WIP: add Canvas component (responsiveness issues).

---
+++ src/components/Canvas.vue
@@ -0,0 +1,130 @@
+<template>
+  <div id="canvas"></div>
+</template>
+
+<script>
+import * as Three from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+
+const scene = new Three.Scene();
+let camera = new Three.PerspectiveCamera()
+let model = null
+const renderer = new Three.WebGLRenderer({ antialias: true });
+
+export default {
+  name: "threejs-test",
+  data() {
+    return {
+      vscene: null,
+      vcamera: null,
+      vrenderer: null,
+      };
+  },
+  props: {
+    modelPath: { type: String, required: true },
+  },
+  methods: {
+    onWindowResize: function () {
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+    },
+    init: function () {
+      const canvas = document.getElementById("canvas");
+
+      this.initScene();
+      this.initLights();
+      this.initCamera();
+      this.initRenderer();
+      this.initControls();
+      this.addGLTFModel();
+
+      canvas.appendChild(renderer.domElement);
+      renderer.render(scene, camera);
+    },
+    initScene: function () {
+      scene.background = new Three.Color(0xffffff);
+      scene.fog = new Three.Fog(0xffffff, 10, 200);
+    },
+    initLights: function () {
+      const ambientLight = new Three.AmbientLight(0xffffff);
+      const pointLight = new Three.PointLight(0xffffff, 1, 100);
+      pointLight.position.set(5, 5, 5);
+      scene.add(ambientLight, pointLight);
+    },
+    initCamera: function () {
+      camera = new Three.PerspectiveCamera(
+        45,
+        canvas.clientWidth / canvas.clientHeight,
+        0.01,
+        100
+      );
+      camera.position.set(0, 0, 12);
+    },
+    initRenderer: function () {
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+    },
+    initControls: function () {
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.minPolarAngle = (Math.PI * 1) / 6;
+      controls.maxPolarAngle = (Math.PI * 3) / 4;
+      controls.smooth = true;
+      controls.enablePan = false;
+      controls.enableZoom = false;
+      controls.enableDamping = true;
+      controls.update();
+    },
+    addGLTFModel: function () {
+      const loader = new GLTFLoader();
+      loader.load(
+        this.modelPath,
+        (gltf) => {
+          model = gltf.scene;
+          model.position.set(0, 0, 0);
+          model.scale.set(2, 2, 2);
+          scene.add(model);
+        },
+        function (progress) {
+          if (progress.loaded / progress.total == 1) {
+            console.log("GLTF LOADED!");
+          }
+        },
+        function (error) {
+          console.error(error);
+        }
+      );
+    },
+    animate: function () {
+      requestAnimationFrame(this.animate);
+      if (model) {
+        model.rotation.y += 0.01;
+      }
+      camera.lookAt(scene.position);
+      renderer.render(scene, camera);
+    },
+  },
+  mounted() {
+    this.init();
+    this.animate();
+    window.addEventListener("resize", this.onWindowResize, false);
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.onWindowResize, false);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#canvas {
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
+  cursor: move;
+}
+</style>
