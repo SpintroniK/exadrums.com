f6b4e37932830d5a352df791edb9a5ec93ae471e
oden.jonathan@gmail.com <oden.jonathan@gmail.com>
2021-08-24 07:31:44 +0200

    Update Latest news component?

--- src/components/LatestNews.vue
+++ src/components/LatestNews.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="news-container">
-      <!-- <p class="news-date">Latest news {{ news.date }}</p> -->
-      <span>📣 Latest News: </span><a :href="news.url" :alt="news.description" class="news-title enable-tooltip">❝ {{ news.title }} ❞</a>
-      <!-- <p>{{ news.summary }}</p> -->
+    <span>{{ news.date }}</span>
+    <div>
+      <a :href="news.url" :alt="news.description" class="news-title">❝ {{ news.title }} ❞</a>
+    </div>
   </div>
 </template>
 
@@ -15,37 +16,55 @@ export default {
 </script>
 
 <style>
+.news-container {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-wrap: wrap;
+  text-align: center;
+  width: 100%;
+}
 
-  .news-container {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: row;
-      gap: var(--spacer);
-      width: max-content;
-      max-width: 80%;
-      margin-top: 5em;
-      margin-left: auto;
-      margin-right: auto;
-      background-color: var(--clr-semi-transparent);
-      backdrop-filter: blur(3px);
-      padding: calc(2 * var(--spacer)) var(--spacer);
-      border-radius: var(--spacer);
-/*       border-left: 4px solid var(--clr-primary);
-      border-right: 4px solid var(--clr-primary); */
-    }
+.news-title {
+  font-size: clamp(1.25em, 3vw, 1.5em);
+  text-decoration: none;
+  color: currentColor;
+  width: max-content;
+  max-width: 60ch;
+}
 
-  .news-date {
-    border-right: 1px dashed var(--clr-secondary);
-    width: min-content;
-    padding-right: var(--spacer);
-  }
-
-  .news-title {
-    position: relative;
-    font-size: clamp(1.25em, 3vw, 1.5em);
-    text-decoration: none;
+@media only screen and (min-width: 85ch) {
+  .news-container  {
+    width: 90%;
+    margin-right: 0;
+    margin-left: auto;
     color: var(--clr-primary);
+    background-color: var(--clr-ternary);
+    padding: calc(var(--spacer) / 2);
+    border-radius: calc(var(--spacer) / 3);
   }
 
+  .news-container:before {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 0;
+    transform: translate(-90%, -50%);
+    border-width: 5px;
+    border-style: solid;
+    border-color: transparent var(--clr-ternary) transparent transparent;
+  }
+
+  .news-container:after {
+    content: url(/assets/megaphone_icon.svg);
+    position: absolute;
+    top: auto;
+    bottom: auto;
+    left: 0;
+    transform: translateX(calc(-100% - 10px));
+    color: var(--clr-ascent);
+  }
+}
 </style>
